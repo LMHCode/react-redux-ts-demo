affa851cf17bd38f260857365f00cc151c7b5d74
liuminghui <liuminghui@haiwang.link>
2020-12-01 16:05:37 +0800

    react脚手架

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import {Router} from 'react-router-dom'
+import { createBrowserHistory } from 'history'
+import { store } from './reducers'
+import Routers from './routers'
+
+const history = createBrowserHistory()
+
+const App: React.FC = () => {
+  return (
+    <Provider store={store}>
+      <Router history={history}>
+        <Routers/>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
